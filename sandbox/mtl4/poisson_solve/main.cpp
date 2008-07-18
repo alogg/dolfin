2a49314a901b7ef4a42baaffd8830396769e2f5c
Garth N. Wells <gnw20@cam.ac.uk>
2008-07-18 19:21:30 +0100

    Work on MTL4.
    Can now solve problems. Only Krylov solver is available.

--- sandbox/mtl4/poisson_solve/main.cpp
+++ sandbox/mtl4/poisson_solve/main.cpp
@@ -6,7 +6,7 @@ using namespace dolfin;
 int main()
 {
   // Create mesh and forms
-  UnitSquare mesh(50, 50);
+  UnitSquare mesh(4, 4);
   DomainBoundary boundary;
 
   Function f(mesh, 2.0);
@@ -30,21 +30,26 @@ int main()
   real ublas_solve = toc();
   cout << "Solve time: " << ublas_solve << endl;
 
+  x_ublas.disp();
+
   // Assemble and solve using MTL4
   printf("MTL4 with specified number of nonzeros----------------------\n");
   Assembler ass_mtl4(mesh); 
   MTL4Matrix A_mtl4(A_ublas.size(0), A_ublas.size(1), 5);
-  MTL4Vector b_mtl4(A_ublas.size(0));
+  MTL4Vector b_mtl4(A_ublas.size(0)), x_mtl4;
 
   tic();
   ass_mtl4.assemble(A_mtl4, a, false); 
   ass_mtl4.assemble(b_mtl4, L, false);
   bc.apply(A_mtl4, b_mtl4, a);
+  ITLKrylovSolver krylov_itl;
+  krylov_itl.solve(A_mtl4, x_mtl4, b_mtl4);
 
 
   real mtl4_solve = toc();
   cout << "Solve time: " << mtl4_solve << endl;
-
+  for(dolfin::uint i = 0; i < x_mtl4.size(); ++i)
+    std::cout<< x_mtl4.vec()[i] << std::endl;
 
   return 0;
 }
