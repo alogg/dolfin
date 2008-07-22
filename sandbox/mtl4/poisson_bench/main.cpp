bce4487e10000f6e032df063817ebb5a65c16e2b
Garth N. Wells <gnw20@cam.ac.uk>
2008-07-22 10:47:23 +0100

    Guess number of non zeroes in MTL4Matrix if not provided.

--- sandbox/mtl4/poisson_bench/main.cpp
+++ sandbox/mtl4/poisson_bench/main.cpp
@@ -1,12 +1,17 @@
+#include "boost/tuple/tuple.hpp"
 #include <dolfin.h>
 #include "Poisson.h"
 
+
 using namespace dolfin;
+
 
 int main(int args, char* argv[])
 {
   if(!argv[1])
     error("This program requires an integer command line arugment, e.g. ./demo 100");   
+
+  Table table;
 
   // Create mesh and forms
   const int N = atoi(argv[1]);
@@ -15,75 +20,42 @@ int main(int args, char* argv[])
   UnitSquare mesh(N, N);
   Function f(mesh,2.0);
   PoissonBilinearForm a;
-  PoissonLinearForm L(f);
 
-  //dolfin::uint NN = (dolfin::uint)sqr(N+1);
-  //cout << "Matrix dimension: " << NN << endl; 
+  Assembler assembler(mesh);
 
-  // Assemble uBLAS matrix and vector
-  cout << "uBLAS ------------------------------------------------------" << endl;
-  Assembler ass_ublas(mesh);
-  Matrix A_ublas;
+  cout << "Assembling uBLAS" << endl; 
+  {
+    uBlasSparseMatrix A;
+    tic(); 
+    assembler.assemble(A, a); 
+    table("uBLAS", "assembly") = toc();
+  }  
 
-  tic(); 
-  ass_ublas.assemble(A_ublas, a); 
-  real ublas_assembly = toc();
-  cout << "Assembly time: " << ublas_assembly << endl;
+  cout << "Assembling MTL4" << endl; 
+  {
+    tic(); 
+    MTL4Matrix A;
+    assembler.assemble(A, a); 
+    table("MTL4", "assembly") = toc();
+  }  
 
-  tic(); 
-  ass_ublas.assemble(A_ublas, a, false); 
-  real ublas_reassembly = toc();
-  cout << "Re-assembly time: " << ublas_reassembly << endl;
+  cout << "Assembling PETSc" << endl; 
+  {
+    tic(); 
+    PETScMatrix A;
+    assembler.assemble(A, a); 
+    table("PETSc", "assembly") = toc();
+  }  
 
-  Vector b_ublas;
-  tic(); 
-  ass_ublas.assemble(b_ublas, L); 
-  real ublas_vector_assembly = toc();
-  cout << "Vector assembly time: " << ublas_vector_assembly << endl;
+  cout << "Assembling Trilinos" << endl; 
+  {
+    tic(); 
+    PETScMatrix A;
+    assembler.assemble(A, a); 
+    table("Trilinos", "assembly") = toc();
+  }  
 
-  // Assemble uBLAS matrix and vector
-  printf("MTL4 with specified number of nonzeros----------------------\n");
-  Assembler ass_mtl4(mesh); 
-  MTL4Matrix A_mtl4(A_ublas.size(0), A_ublas.size(1), 5);
-
-  tic(); 
-  ass_mtl4.assemble(A_mtl4, a, false); 
-  real mtl4_assembly_spec = toc();
-  cout << "Assembly time: " << mtl4_assembly_spec << endl;
-
-  tic(); 
-  ass_mtl4.assemble(A_mtl4, a, false); 
-  real mtl4_reassembly_spec = toc();
-  cout << "Re-assembly time: " << mtl4_reassembly_spec << endl;
-
-  // Assemble MTL4 matrix and vector
-  printf("MTL4 without specified numer of nonzeros---------------------\n");
-  Assembler ass_mtl4b(mesh); 
-  MTL4Matrix A_mtl4b(A_ublas.size(0), A_ublas.size(1));
-
-  tic(); 
-  ass_mtl4b.assemble(A_mtl4b, a, false); 
-  real mtl4_assembly = toc();
-  cout << "Assembly time: " << mtl4_assembly << endl;
-
-  tic(); 
-  ass_mtl4b.assemble(A_mtl4b, a, false); 
-  real mtl4_reassembly = toc();
-  cout << "Re-assembly time: " << mtl4_reassembly << endl;
-
-  MTL4Vector b_mtl4b(A_ublas.size(0));
-  tic();
-  ass_mtl4b.assemble(b_mtl4b, L, false);
-  real mtl4_vector_assembly = toc();
-  cout << "Vector assembly time: " << mtl4_vector_assembly << endl;
-
-
-  cout << "-------------------------------------" << endl;
-  cout << "Summary:          MTL4 (a), MTL4 (b), uBLAS" << endl;
-  cout << "First matrix assembly:   " << mtl4_assembly        << "  " << mtl4_assembly_spec   << "  " << ublas_assembly        << endl;
-  cout << "Matrix re-assembly:      " << mtl4_reassembly      << "  " << mtl4_reassembly_spec << "  " << ublas_reassembly      << endl;
-  cout << "Vector    assembly:      " << mtl4_vector_assembly << "  " << mtl4_vector_assembly << "  " << ublas_vector_assembly << endl;
-  cout << "Matrix/vector dimension: " << A_ublas.size(0) << endl;
+  table.disp();
 
   return 0;
 }
