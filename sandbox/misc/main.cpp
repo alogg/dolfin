6915a42bb99ef22d854771fd72cb328250635e3b
Anders Logg <logg@simula.no>
2008-11-14 21:24:28 +0100

    Fix bug in init: only init when needed. Unit tests work again now.

--- sandbox/misc/main.cpp
+++ sandbox/misc/main.cpp
@@ -1,14 +1,11 @@
 // Place for random tests
 
 #include <dolfin.h>
-#include "Poisson.h"
 
 using namespace dolfin;
 
 int main()
 {
-  std::tr1::shared_ptr<Vector> x(new Vector);
-
-  std::cout << "Testing " << x << std::endl; 
-
+  UnitSquare mesh(2, 2);
+  BoundaryMesh boundary(mesh);
 }
