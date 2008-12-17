bd6a0a6f3748735b30b15ee75a87d60cda8f1f97
Ola Skavhaug <skavhaug@simula.no>
2008-12-17 16:10:17 +0100

    MeshPartitioning now works. No deltas computed yet :)

--- sandbox/passembly/main.cpp
+++ sandbox/passembly/main.cpp
@@ -3,6 +3,7 @@
 // To run this demo, make sure to build DOLFIN with the flag customCxxFlags=-DHAS_PARMETIS=1
 
 #include <dolfin.h>
+#include <dolfin/main/MPI.h>
 
 using namespace dolfin;
 
@@ -16,6 +17,13 @@ int main(int argc, char* argv[])
   // Partition mesh
   Mesh mesh;
   MeshPartitioning::partition(mesh, data);
+
+  dolfin::uint process_number = dolfin::MPI::process_number();
+  char c[100];
+  sprintf(c, "mesh_part_%d.xml.gz", process_number);
+  File outfile(c);
+
+  outfile << mesh;
 
   return 0;
 }
