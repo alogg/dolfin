55e4f0228790f4d659f32c0a75e7c57a9f9a1dee
Garth N. Wells <gnw20@cam.ac.uk>
2009-02-08 13:53:30 +0000

    Add example to sandbox/misc which causes nasty memory leak.
    Problem seems to be in GTSInterface.

--- sandbox/misc/main.cpp
+++ sandbox/misc/main.cpp
@@ -1,40 +1,85 @@
-// Place for random tests
+// Copyright (C) 2009 Marc Spiegelman
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2008-10-08
+// Last changed:  5 Feb 2009 17:16:07
+//
+// Heavily modified from dolfin/demo/mesh/intersection code
+// Program to demonstrated memory leak from  large number of random GTS intersectionDetector tests
+// uses boost uniform random number generator for fun
+// 
+// Note: if you just continually test the point {0.5, 0.5} (comment out the random number generator)
+//  the number of intersected cells is 6 rather than 1 and the memory leak is much more pronounced
+// 
 
 #include <dolfin.h>
+#include <boost/random.hpp>
 
 using namespace dolfin;
 
+typedef boost::mt19937 base_generator_type;
+
 int main()
 {
-  // Create meshes
-  //UnitCircle mesh0(10);
-  UnitSquare mesh1(5,5);  
-  UnitSquare mesh0(2,2);  
+  // Create square mesh
+  unsigned int N = 64;
+  UnitSquare mesh(N, N);
+
+  //create IntersectionDetector
+  IntersectionDetector ID(mesh);
     
-  cout << "***************HEJ**************" << endl;
+  // set initial point
+  double x[2] = {0.5, 0.5 };
+  Point p(2, x);
 
-  // Move and scale circle
-   MeshGeometry& g = mesh0.geometry();
-  for (VertexIterator vertex(mesh0); !vertex.end(); ++vertex)
-  {
-//     double* x = g.x(vertex->index());
-//     x[0] = 0.5*x[0] + 1.05;
-//     x[1] = 0.5*x[1] + 1.05;
+  //initialize cell Array
+  std::vector<dolfin::uint> cells;
+  std::vector<dolfin::uint>::iterator cellIterator;
+
+  //setup boost uniform variate random number generators
+  base_generator_type  generator(42u);
+  boost::uniform_real<> uni_dist(0,1);
+  boost::variate_generator<base_generator_type&, boost::uniform_real<> > uni(generator, uni_dist);
   
-    double* x = g.x(vertex->index());
-    x[0] = x[0] + 0.75;
-    x[1] = x[1] + 0.55; //0.60
+  dolfin::uint k, k_max = N*N*5000, k_print = 100;
+  dolfin::uint n_cells, n_cells_print=2;
+
+  //calculate a random intersection k_max times
+  for (k = 0; k < k_max; k++ ) 
+  {
+    ID.intersection(p, cells);
+    n_cells = cells.size();
+
+    //print out information if number of cells in intersection matches n_cells_print (or every k_print grid_sweep)
+    if (n_cells >= n_cells_print || k%(N*N*k_print) == 0 ) 
+    {
+      printf("k=%d, p=[%f, %f], NCells=%d:  ", k, p.x(), p.y(), (int) cells.size()) ;
+      for (cellIterator = cells.begin(); cellIterator < cells.end(); cellIterator++) 
+      {
+        printf("%d ",*cellIterator);
+      }
+      printf("\n");
+    }
+
+    // get new random point (comment out to really blow up)
+    p[0] = uni();
+    p[1] = uni();
+
+    // clear cells array (cells  grows with every test otherwise, not sure if this generates a memory leak)
+    cells.clear(); 
   }
-
-  // Compute intersection with boundary of square
-  BoundaryMesh boundary1(mesh1);
-  Array<unsigned int> cells0;
-  mesh0.intersection(boundary1, cells0);
-
-  // File f0("mesh0.pvd");
-  //File f1("mesh1.pvd");
-  //f0 << mesh0;
-  //f1 << mesh1;
+  return(0);
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
