fb9a035b85d9f707d853c021a4415be82b858da2
Anders Logg <logg@simula.no>
2009-02-17 17:19:20 +0100

    Add demo of interpolation to vector in sandbox

--- sandbox/misc/main.cpp
+++ sandbox/misc/main.cpp
@@ -1,29 +1,37 @@
-#include <boost/numeric/mtl/mtl.hpp>
-#include <boost/numeric/itl/itl.hpp>
+// Place for random tests
+
+#include <dolfin.h>
+#include "Poisson.h"
+
+using namespace dolfin;
+
+class MyFunction : public Function
+{
+public:
+
+  MyFunction(const FunctionSpace& V) : Function(V) {};
+
+  void eval(double* values, const double* x) const
+  {
+    message("Calling eval");
+    double dx = x[0] - 0.5;
+    double dy = x[1] - 0.5;
+    values[0] = 500.0*exp(-(dx*dx + dy*dy) / 0.02);
+  }
+};
 
 int main()
 {  
-  mtl::compressed2D<double> A(10, 10);
-  mtl::dense_vector<double> x(10);
-  mtl::dense_vector<double> f(10);
+  UnitSquare mesh(2, 2);
+  PoissonFunctionSpace V(mesh);
+  MyFunction f(V);
+  Vector x;
+  
+  message("Interpolating to another vector");
+  f.interpolate(x, f.function_space());
+  x.disp();
 
-  itl::basic_iteration<double> iter(f, 500, 1.0e-6);
-
-  itl::pc::ilu_0<mtl::compressed2D<double> > P(A);
-  //itl::pc::ic_0<mtl::compressed2D<double> > P(A);
-
-  itl::cg(A, x, f, P, iter);
+  message("Interpolating to the function vector");
+  f.interpolate(f.vector(), f.function_space());
+  f.vector().disp();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
