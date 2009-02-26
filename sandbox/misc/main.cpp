34b116070ee97366744996ad4fcb4d2ad03f99e7
Garth N. Wells <gnw20@cam.ac.uk>
2009-02-26 23:01:39 +0000

    Sandbox change.

--- sandbox/misc/main.cpp
+++ sandbox/misc/main.cpp
@@ -4,22 +4,21 @@ using namespace dolfin;
 
 int main (int argc, char **argv) {
 
-   UnitCube mesh(10,10,10);
+   UnitCube mesh(10, 10, 10);
 
    FiniteElement fe("FiniteElement('Lagrange', 'tetrahedron', 1)");
    DofMap dofmap("FFC dof map for FiniteElement('Lagrange', 'tetrahedron', 1)", mesh);
 
-   Function j( FunctionSpace(mesh, fe, dofmap) );
+   FunctionSpace V(mesh, fe, dofmap);
+   Function j( V );
 
    j.vector() = 1.0;
 
+   Point point(0.5, 0.5, 0.5);
+   double x[3];
 
-   Point point( 0.5, 0.5, 0.5);
-   Point value;
-
-   j.eval((double *)value.coordinates(), point.coordinates());
-
-   std::cout << value[0] << std::endl;
+   j.eval(x, point.coordinates());
+   std::cout << x[0] << std::endl;
 
    return 0;
 } 
