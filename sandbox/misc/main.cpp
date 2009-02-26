13875be35f4fb236d6e416c8bc9d23df984b872b
Garth N. Wells <gnw20@cam.ac.uk>
2009-02-26 20:12:19 +0000

    Add test program from BArtek.

--- sandbox/misc/main.cpp
+++ sandbox/misc/main.cpp
@@ -1,32 +1,25 @@
-// Place for random tests
-
 #include <dolfin.h>
-#include "Poisson.h"
 
 using namespace dolfin;
 
-class Image {};
+int main (int argc, char **argv) {
 
-class ImageMesh : public Mesh
-{
-public:
-  
-  ImageMesh(const Image& image) : Mesh()
-  {
-    // Compute nx, ny etc
-    unsigned int nx = 10;
-    unsigned int ny = 10;
+   UnitCube mesh(10,10,10);
 
-    // Create unit square mesh and copy it
-    UnitSquare mesh(nx, ny);
-    static_cast<Mesh&>(*this) = mesh;
-  }
-};
+   FiniteElement fe("FiniteElement('Lagrange', 'tetrahedron', 1)");
+   DofMap dofmap("FFC dof map for FiniteElement('Lagrange', 'tetrahedron', 1)", mesh);
 
-int main()
-{
-  Image image;
-  ImageMesh mesh(image);
+   Function j( FunctionSpace(mesh, fe, dofmap) );
 
-  mesh.disp();
-}
+   j.vector() = 1.0;
+
+
+   Point point( 0.5, 0.5, 0.5);
+   Point value;
+
+   j.eval((double *)value.coordinates(), point.coordinates());
+
+   std::cout << value[0] << std::endl;
+
+   return 0;
+} 
