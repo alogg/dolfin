924e9f4d757b08f1b4c6c76956d4ddd5aefeb20a
Anders Logg <logg@simula.no>
2009-03-11 23:04:14 +0100

    Add sandbox test script for function assignment

---
+++ sandbox/misc/python/test.py
@@ -0,0 +1,56 @@
+from dolfin import *
+
+def test1():
+    "Test time-stepping with initial data expression."
+
+    # Initializations
+    mesh = UnitSquare(32, 32)
+    V = FunctionSpace(mesh, "Lagrange", 1)
+    u0 = Function(V, "sin(x[0])")
+    u1 = Function(V)
+
+    # One of these needed
+    #u0 = interpolate(u0, V)
+    #u0 = project(u0, V)
+    #u0.interpolate()
+
+    # Time stepping
+    for i in range(10):
+
+        print i
+
+        # Solve for u1
+        u1.vector()
+
+        # Assign u0 = u1 (requires interpolation/projection)
+        u0.assign(u0)
+
+def test2():
+    "Test time-stepping with time-dependent coefficient expression."
+
+    # Initializations
+    mesh = UnitSquare(32, 32)
+    V = FunctionSpace(mesh, "Lagrange", 1)
+    w0 = Function(V)
+    w1 = Function(V, "sin(t*x[0])")
+
+    # Time stepping
+    for i in range(10):
+
+        print i
+
+        # Update w1
+        w1.t = float(i)
+        
+        # Solve for u
+
+        # One of these needed
+
+        # Assign w0 = w1 (does not work)
+        w0.assign(w1)
+        #w0 = interpolate(w1, V)
+        #w0 = project(w1, V)
+
+#test1()
+test2()
+
