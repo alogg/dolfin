8653b4aab955cf05bb838c50a476c17f7d59c555
Garth N. Wells <gnw20@cam.ac.uk>
2009-06-15 08:24:02 +0100

    Add interpolation on non-matching grids example to sandbox.

--- sandbox/nonmatching/main.cpp
+++ sandbox/nonmatching/main.cpp
@@ -1,60 +1,46 @@
+// Copyright (C) 2009 Garth N. Wells.
 // Licensed under the GNU LGPL Version 2.1.
 
 #include <dolfin.h>
-#include "Simple.h"
-  
+#include "P1.h"
+
 using namespace dolfin;
 
-int main(int argc, char* argv[])
+class MyFunction : public Function
 {
-  Mesh meshA;
-  MeshEditor editor;
-  editor.open(meshA, "triangle", 2, 2);
-  editor.initVertices(4);
-  editor.addVertex(0, 0.0, 0.0);
-  editor.addVertex(1, 1.0, 0.0);
-  editor.addVertex(2, 1.0, 1.0);
-  editor.addVertex(3, 0.0, 1.0);
-  editor.initCells(2);
-  editor.addCell(0, 0, 1, 3);
-  editor.addCell(1, 1, 2, 3);
-  editor.close();
+  public:
 
-  Function fA;
-  Vector xA;
+  MyFunction(FunctionSpace& V) : Function(V) {}
 
-  Form* M = new SimpleFunctional(fA);
-
-  fA.init(meshA, xA, *M, 0);
-
-  int NA = meshA.numVertices();
-
-  // Generate some values for fA
-  double* arr = new double[NA];
-  for (VertexIterator v(meshA); !v.end(); ++v)
+  void eval(double* values, const double* x) const
   {
-    int id = v->index();
-    Point p = v->point();
-
-    arr[0 * NA + id] = 2.0 * (p[1] - 0.5) * pow(p[0] - 0.0, 2); 
+    double dx = x[0] - 0.5;
+    double dy = x[1] - 0.5;
+    values[0] = 500.0*exp(-(dx*dx + dy*dy) / 0.02);
   }
+};
 
-  fA.vector().set(arr);
-  delete arr;
 
-  File file_fA("fA.pvd");
-  file_fA << fA;
+int main()
+{
+  UnitSquare mesh0(16, 16);
+  UnitCube mesh1(32, 32, 32);
 
-  UnitSquare meshB(11, 11);
+  P1::FunctionSpace V0(mesh0);
+  P1::FunctionSpace V1(mesh1);
 
-  Function fB;
+  MyFunction my_function0(V0);
+  my_function0.interpolate();
 
-  ufc::finite_element* element = M->form().create_finite_element(0);
+  Vector x; 
+  V1.interpolate(x, my_function0);
 
-  projectL2NonMatching(meshB, fA, fB, *element);
+  Function my_function1(V1, x);
 
-  File file_fB("fB.pvd");
-  file_fB << fB;
+  //my_function1.interpolate(x, V1);
+
+  plot(my_function0);
+  plot(my_function1);
 
   return 0;
 }
