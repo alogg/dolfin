c8478c66c2d176e3b8215aa5c96a16cbb835f553
Anders Logg <logg@simula.no>
2009-11-10 18:00:46 +0100

    Return more informative error message when function evaluation fails.

--- sandbox/misc/cpp/main.cpp
+++ sandbox/misc/cpp/main.cpp
@@ -9,10 +9,12 @@ int main (int argc, char* argv[])
   UnitSquare mesh(3, 3);
 
   Poisson::FunctionSpace V(mesh);
-
+  Function v(V);
+  mesh.refine();
   mesh.refine();
 
-  Function v(V);
+  return 0;
+
   GenericVector& x = v.vector();
   for (dolfin::uint i = 0; i < V.dim(); i++)
     x.setitem(i, static_cast<double>(i));
