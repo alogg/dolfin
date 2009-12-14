b2f114463fad17552c75c3e4651875ef7f89cdb2
Anders Logg <logg@simula.no>
2009-12-14 21:35:57 +0100

    Add initial version of reconstruction operator. Currently failing when linking sandbox demo because of missing implementation of dgelss. Does not seem to be available in ATLAS. Might need TAUCS or LAPACK_USE_F2C.

--- sandbox/misc/main.cpp
+++ sandbox/misc/main.cpp
@@ -1,10 +1,33 @@
 #include <dolfin.h>
 #include "Poisson.h"
+#include "P1.h"
+#include "P2.h"
 
 using namespace dolfin;
 
+class MyExpression : public Expression
+{
+  void eval(Array<double>& values, const Array<const double>& x) const
+  {
+    values[0] = sin(3.0*x[0]);
+  }
+};
+
 int main()
 {
+  // Create function spaces
+  UnitSquare mesh(4, 4);
+  P1::FunctionSpace V(mesh);
+  P2::FunctionSpace W(mesh);
+
+  // Create P1 function
+  MyExpression f;
+  Function v(V);
+  v.interpolate(f);
+
+  // Create P2 reconstruction
+  Function w(W);
+  w.reconstruct(v);
 
   return 0;
 }
